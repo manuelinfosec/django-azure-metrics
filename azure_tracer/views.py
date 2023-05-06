30423c7deb5362265abc2eb85dd62dc34e5d1e29
Manuel <manuelinfosec@gmail.com>
2023-05-06 03:32:17 +0100

    CI, Docker, Serializers & Views

--- azure_tracer/views.py
+++ azure_tracer/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
