4f8a29f59e66fcb0154b2e00c8dc50a28904c3c6
Manuel <manuelinfosec@gmail.com>
2023-07-13 23:02:03 +0100

    Settings: Update ref to auth_manager

--- azure_django/settings.py
+++ azure_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "azure_tracer.apps.AppConfig"
 ]
 
 MIDDLEWARE = [
--- azure_django/urls.py
+++ azure_django/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from azure_tracer import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   ("", include(router.urls)) 
 ]
